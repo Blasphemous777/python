049d1eea6335e1061d8221ab42b238d34a835c72
Blasphemous777 <saintelvis@yandex.ru>
2023-08-08 18:07:48 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,22 +1,13 @@
 import requests
 
-url = 'https://wttr.in/san%20francisco?lang=ru&?M?m?n?q?T'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+def get_weather(city):
+    url = 'https://wttr.in/' + city + '?lang=ru&?M?m?n?q?T'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+    
+cities = ['san%20francisco', 'Череповец', 'Шереметьево', 'Лондон']
 
-
-url = 'https://wttr.in/Череповец?lang=ru&?M?m?n?q?T'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
-
-url = 'https://wttr.in/Шереметьево?lang=ru&?M?m?n?q?T'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
-
-url = 'https://wttr.in/Лондон?lang=ru&?M?m?n?q?T'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+if __name__ == "__main__":
+    for i in cities:
+        print(get_weather(i))
